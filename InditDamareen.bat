0a68542cf9fc6ff2a453eb35be5a442d396ef00e
Zalihun69 <159771111+Zalihun69@users.noreply.github.com>
2025-11-09 15:12:46 +0100

    Add files via upload

---
+++ InditDamareen.bat
@@ -0,0 +1,13 @@
+@echo off
+REM ==============================
+REM Damareen játék indító fájl
+REM ==============================
+
+REM A mappa, ahol a Python fájl van
+C:\Users\user\Documents\dusza verseny\dusza_verseny25-26-main
+
+REM Futtatás Python-nal
+python ui.py
+
+echo.
+pause
